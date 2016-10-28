94d7ea6ddafc8b133aaf2330c416f5ccfd38116e
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2016-10-28 16:12:29 +0900

    update

--- tester.py
+++ tester.py
@@ -49,7 +49,7 @@ ulimit -n 65536''']
                     if conf.mpls:
                         #route 10.0.0.0/24 rd 65000:1 next-hop 200.10.0.101 extended-community [ 0x0002fde800000001 0x0002271000000001 ] label 1000 split /25 ;
                         f.write('      route {0} rd {1}:{2} next-hop {3} extended-community [ 0x0002fde800000001 0x0002271000000001 ] label {4} split /25 ;\n'.format(path, p['as'], (1000+i), local_address, (1000+i)))
-                    else
+                    else:
                         f.write('      route {0} next-hop {1};\n'.format(path, local_address))
                 f.write('''   }
 }
