501715f03fcdcd299e9c016d579b70c98cb98b60
Hiroshi Yokoi <yokoi.hiroshi@po.ntts.co.jp>
2016-10-28 16:17:26 +0900

    update

--- bgperf.py
+++ bgperf.py
@@ -357,7 +357,7 @@ def gen_conf(args):
         }
 
         if args.use_vpn:
-            conf['tester'][router_id]['vrf'] = {'rd': peer_as+":"+(1000 + i), 'rt': peer_as+":"+(1000 + i)}
+            conf['tester'][router_id]['vrf'] = {'rd': peer_as+":"+str(1000 + i), 'rt': peer_as+":"+str(1000 + i)}
 
     return conf
 
